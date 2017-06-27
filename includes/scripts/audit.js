30230439b2a53aa982ba0d5daff6e8858e90d936
Banzboy <bobh4ring@gmail.com>
2017-06-27 23:06:57 +0200

    First Commit

---
+++ includes/scripts/audit.js
@@ -0,0 +1,16 @@
+
+var config = JSON.parse();
+
+
+// Read program config with custom settings to determine what the application will do
+
+// Run program based on CLI 
+// Example commands:
+// - audit http://www.bobharing.nl/
+// - audit bobharing
+//      - bobharing results with reading a json file which has the all the urls for the website that are to be audited
+
+// Place JSON results in correct directories
+// Read JSON results and with VUE display results in a web interface.
+// Compare metrics of two audits with each other with visualization of said metrics 
+
