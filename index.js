d1ea8333bcc8ea46ce7b538389aa2c1db787a921
Banzboy <bobh4ring@gmail.com>
2017-07-09 12:34:20 +0200

    Refactored run multiple tests command

--- index.js
+++ index.js
@@ -15,18 +15,84 @@
 // Compare metrics of two audits with each other with visualization of said m
 
 /// Load modules
-var program = require('commander');
+const program = require('commander');
+const _ = require('lodash');
 const fs = require('fs');
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('lighthouse/chrome-launcher');
+const path = require('path');
+const mkdirp = require('mkdirp');
+
+const flags = { output: 'json' };
+
+
+const templatePath = "./site_templates/";
+const resultPath = "./json_results/";
 
 program
     .option('-u, --url <url>', 'The url to audit')
     .option('-s, --site <site>', 'The site to audit')
     .parse(process.argv);
 
+
 if (program.url) console.log("The following url will be audited" + program.url);
-if (program.site) console.log("The following site will be audited" + program.site);
+if (program.site) {
+    var promise = new Promise(function (resolve, reject) {
+        fs.readdir(templatePath, function (err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                var siteTemplates = [];
+                var count = 0;
+
+                data = _.reject(data, dataItem => /\.gitignore/.test(dataItem));
+
+                var dataCount = data.length;
+
+                data.forEach(function (siteTemplateFile) {
+                    fs.readFile(templatePath + siteTemplateFile, 'utf8', function (err, data) {
+                        if (err) {
+                            reject(err + "Unable to read site template file.");
+                        } else {
+
+                            siteTemplates = siteTemplates.concat(JSON.parse(data));
+                            count++;
+
+                            if (count == dataCount) {
+                                if (siteTemplates.length > 0) {
+                                    resolve(siteTemplates);
+                                }
+                            }
+                        }
+                    });
+                });
+            }
+        });
+    });
+
+    promise.then(function (sitesSettings) {
+        if (program.site == "all") {
+
+            _.forEach(sitesSettings, function (site) {
+                _.forEach(site.page, function (page) {
+                    createJSONResult(site.url, page.path, page.pagename);
+                });
+            });
+        } else {
+            var selectedSite = _.filter(sitesSettings, ['sitename', program.site]);
+
+            console.log(selectedSite);
+            for (var i = 0; i < page.length; i++) {
+                console.log("bob")
+                if (typeof a[i].hasOwnProperty(key)) {
+                    console.log(a[i][key]);
+                }
+                //createJSONResult(selectedSite.url, selectedSite.url, page);
+            }
+        }
+    })
+
+}
 
 function launchChromeAndRunLighthouse(url, flags = {}, config = null) {
     return chromeLauncher.launch().then(chrome => {
@@ -36,48 +102,30 @@ function launchChromeAndRunLighthouse(url, flags = {}, config = null) {
     });
 }
 
-const flags = { output: 'json' };
 
-// Usage:
-launchChromeAndRunLighthouse(program.url, flags).then(results => {
-    // Use results!
+function createJSONResult(rootUrl, pageUrl, pageName) {
+    var testURL = rootUrl + pageUrl;
+    testURL = testURL.replace(/([^:]\/)\/+/g, "$1");
 
-    // TODO: Read template values to run audits per page on a site.
+    launchChromeAndRunLighthouse(testURL, flags).then(results => {
+        // Use results!
+        var jsonResults = JSON.stringify(results);
+        // This solution is disgusting make it to one regex!!!
+        var domainName = rootUrl.replace(/(http(s?))\:\/\//, "").replace(/(\/)/, "").replace(/\./g, "_");
 
-    // This solution is disgusting make it to one regex!!!
-    var domainName = program.url.replace(/(http(s?))\:\/\//, "").replace(/(\/)/, "").replace(/\./g, "_");
+        var jSONResultPath = resultPath + domainName + "/" + pageName;
 
-    var resultPath = "./json_results/" + domainName;
-
-    ensureExists(resultPath, 0744, function (e) {
-        if (e) {
-            console.log("Existing site directory found.");
-        }
-        else {
-            fs.mkdir(resultPath, function (err) {
-                if (err) {
-                    console.log("Directory not found.")
-                } else {
-                    fs.writeFile(resultPath + "/" + domainName + "_" + Date.now() + '.json', results, function (e) {
-                        if (e) {
-                            return console.log(e);
-                        }
-                    });
-                }
-            });
-        }
-    })
-});
-
-function ensureExists(path, mask, cb) {
-    if (typeof mask == 'function') { // allow the `mask` parameter to be optional
-        cb = mask;
-        mask = 0777;
-    }
-    fs.mkdir(path, mask, function (err) {
-        if (err) {
-            if (err.code == 'EEXIST') cb(null); // ignore the error if the folder already exists
-            else cb(err); // something else went wrong
-        } else cb(null); // successfully created folder
+        mkdirp(jSONResultPath, 0744, function (e) {
+            if (e) {
+                console.log(e);
+            }
+            else {
+                fs.writeFile(jSONResultPath + "/" + domainName + "_" + Date.now() + '.json', jsonResults, function (e) {
+                    if (e) {
+                        return console.log(e);
+                    }
+                });
+            }
+        })
     });
 }
